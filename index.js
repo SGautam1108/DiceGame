c54be40a3d4b6fb29797e664b8307c63925340df
SGautam1108 <shauryagautam12a@gmail.com>
2022-01-06 22:48:53 +0530

    Added edit Names functionality

--- index.js
+++ index.js
@@ -9,18 +9,31 @@ function myFunction(){
     img1 = document.querySelectorAll(".die-img")[0],
     img2 = document.querySelectorAll(".die-img")[1],
     mainTitle = document.querySelector(".page-title"),
+    player1 = document.querySelectorAll(".player-title")[0],
+    player2 = document.querySelectorAll(".player-title")[1],
     victoryMessage = "";
     
     img1.setAttribute("src", "images/dice" + random1 + ".png");
     img2.setAttribute("src", "images/dice" + random2 + ".png");
     
     if(random1 > random2){
-        victoryMessage = "<i class='fas fa-flag'></i> Player 1 Wins";
+        victoryMessage = "<i class='fas fa-flag'></i> " + player1.textContent + " Wins";
     } else if(random2 > random1){
-        victoryMessage = "Player 2 Wins <i class='fas fa-flag'></i>";
+        victoryMessage = player2.textContent + "Wins <i class='fas fa-flag'></i>";
     } else {
         victoryMessage = "That's a draw";
     }
     
     mainTitle.innerHTML = victoryMessage;
+}
+
+function editNames(){
+    var
+    player1 = document.querySelectorAll(".player-title")[0],
+    player2 = document.querySelectorAll(".player-title")[1],
+    name1 = prompt("Enter Name of P1"),
+    name2 = prompt("Enter Name of P2");
+
+    player1.innerHTML = name1;
+    player2.innerHTML = name2;
 }
